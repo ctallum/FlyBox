a3597e103f2511d1f4925c051c626ec1415b3dba
Rebecca Flach <beccablockflach@gmail.com>
2023-03-08 16:46:14 -0500

    download modal fixes

--- programming-gui/src/App.tsx
+++ programming-gui/src/App.tsx
@@ -244,8 +244,8 @@ function App() {
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "15px" }}>
                 <input
                     value={downloadName}
-                    onKeyDown={(e) => e.key === "Enter" && downloadData()}
-                    onChange={(e) => setDownloadName(e.target.value)}
+                    onKeyDown={(e) => { e.stopPropagation(); e.key === "Enter" && downloadData() }}
+                    onChange={(e) => { setDownloadName(e.target.value) }}
                     autoFocus
                     onFocus={(e) => e.target.select()}
                     className="text-input"
