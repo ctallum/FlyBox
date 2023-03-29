b06e74ce0d6bb7fc630692578c424da4d082f2eb
Chris Allum <ctallum@gmail.com>
2023-03-29 09:50:28 -0400

    move firmware to submodule

--- firmware/src/screens/select.h
+++
@@ -1,6 +0,0 @@
-#ifndef select_h
-#define select_h
-
-char* selectFiles(fs::FS& fs, ESP32Encoder encoder);
-
-#endif
--- firmware/src/screens/status.h
+++
@@ -1,8 +0,0 @@
-#ifndef status_h
-#define status_h
-
-void updateStatusDisplay(Event* event, PinStatus* Pins[3]);
-void initStatus();
-void updateStatusPercentAndTime(int currentMinute, int startMinute, int endMinute, Time* currnetTime);
-
-#endif
